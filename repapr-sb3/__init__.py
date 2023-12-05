b3cb4c60a612e10f3be03eb4d9ec402677ba4ee5
solightail <solightail238@outlook.jp>
2023-12-06 01:22:39 +0900

    Fix: [All] override & db reward

--- repapr-sb3/__init__.py
+++ repapr-sb3/__init__.py
@@ -2,12 +2,12 @@ from gymnasium.envs.registration import register
 from gymnasium.wrappers.autoreset import AutoResetWrapper
 from .modules.conf import Conf
 
-conf_filepath = "repapr-ppo-cas/config.toml"
+conf_filepath = "repapr-sb3/config.toml"
 cfg = Conf(conf_filepath)
 
 register(
     id='repapr-v0',
-    entry_point='repapr-ppo-cas.modules.env:RePAPREnv',
+    entry_point='repapr-sb3.modules.env:RePAPREnv',
     max_episode_steps=cfg.max_episode_steps,
     additional_wrappers=(AutoResetWrapper.wrapper_spec(),)
 )
